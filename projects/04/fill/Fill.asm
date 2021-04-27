5eb06a7b91c9c5b8309f4c320dcad55afa0504ee
nfukasawa <get.it.on.fuka@gmail.com>
2021-04-27 20:10:11 +0900

    add fill.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(LOOP)
+    // if @KBD > 0 then goto FILL
+    @KBD
+    D=M
+    @FILL
+    D;JGT
+
+(CLEAR)
+    // n = 512*256/16
+    @8192
+    D=A
+
+(CLEAR_LOOP)
+    // n--
+    D=D-1
+
+    // *(@SCREEN+n)=0x0000
+    @SCREEN
+    A=A+D
+    M=0
+    
+    // if n >= 0 then goto CLEAR_LOOP
+    @CLEAR_LOOP
+    D;JGE
+
+    // go to LOOP
+    @LOOP
+    0;JMP
+    
+(FILL)
+    // n = 512*256/16
+    @8192
+    D=A
+
+(FILL_LOOP)
+    // n--
+    D=D-1
+
+    // *(@SCREEN+n)=0xFFFF
+    @SCREEN
+    A=A+D
+    M=-1
+
+    // if n >= 0 then goto FILL_LOOP
+    @FILL_LOOP
+    D;JGE
+
+    // go to LOOP
+    @LOOP
+    0;JMP
